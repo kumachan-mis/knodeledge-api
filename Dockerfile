05e549b1575f1fb9c4f57f7fda28bf1c82947fa0
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2024-01-07 15:36:09 +0900

    feat: deploy Hello World App (#7)

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM golang:1.21-alpine AS builder
+
+WORKDIR /app
+COPY cmd/      cmd/
+COPY internal/ internal/
+COPY go.sum    go.sum
+COPY go.mod    go.mod
+
+RUN go build -o app cmd/app/main.go
+
+
+FROM scratch AS runner
+
+ARG ENVIRONMENT
+ARG ALLOW_ORIGIN
+ARG TRUSTED_PROXY
+
+ENV GIN_MODE=release \
+    ENVIRONMENT=$ENVIRONMENT \
+    ALLOW_ORIGIN=$ALLOW_ORIGIN \
+    TRUSTED_PROXY=$TRUSTED_PROXY
+
+COPY --from=builder /app/app /app/app
+
+EXPOSE 8080
+
+ENTRYPOINT ["/app/app"]
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/kumachan-mis/knodeledge-api/interal/api"
+	"github.com/kumachan-mis/knodeledge-api/internal/api"
 	"github.com/subosito/gotenv"
 )
 
@@ -18,6 +19,12 @@ func main() {
 	gotenv.Load(fmt.Sprintf(".env.%s", env))
 
 	router := gin.Default()
+	router.SetTrustedProxies([]string{os.Getenv("TRUSTED_PROXY")})
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{os.Getenv("ALLOW_ORIGIN")}
+	router.Use(cors.New(config))
+
 	router.GET("/", func(cxt *gin.Context) {
 		cxt.JSON(200, gin.H{
 			"environment": env,
@@ -28,5 +35,5 @@ func main() {
 
 	router.POST("/api/hello-world", api.HelloWorldHandler)
 
-	router.Run(fmt.Sprintf(":%s", os.Getenv("APP_PORT")))
+	router.Run(":8080")
 }
