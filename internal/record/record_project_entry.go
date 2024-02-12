2c40c19484e6e6ae1e578fe0e3014251c3c1844e
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-02-12 12:21:16 +0900

    update test fixtures

--- internal/record/record_project_entry.go
+++ internal/record/record_project_entry.go
@@ -2,6 +2,6 @@ package record
 
 type ProjectEntry struct {
 	Name        string `firestore:"name"`
-	Description string `firestore:"description"`
+	Description string `firestore:"description,omitempty"`
 	UserId      string `firestore:"userId"`
 }
