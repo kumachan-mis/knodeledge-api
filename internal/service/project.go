b3441d92ffed8ba8cb248b4e39a3f0839f62acf4
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-02-24 17:44:22 +0900

    implement service

--- internal/service/project.go
+++ internal/service/project.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 
 	"github.com/kumachan-mis/knodeledge-api/internal/domain"
+	"github.com/kumachan-mis/knodeledge-api/internal/record"
 	"github.com/kumachan-mis/knodeledge-api/internal/repository"
 )
 
@@ -12,6 +13,7 @@ import (
 
 type ProjectService interface {
 	ListProjects(userId domain.UserIdObject) ([]domain.ProjectEntity, error)
+	CreateProject(userId domain.UserIdObject, project domain.ProjectWithoutAutofieldEntity) (*domain.ProjectEntity, error)
 }
 
 type projectService struct {
@@ -66,4 +68,44 @@ func (s projectService) ListProjects(userId domain.UserIdObject) ([]domain.Proje
 	})
 
 	return projects, nil
+}
+
+func (s projectService) CreateProject(userId domain.UserIdObject, project domain.ProjectWithoutAutofieldEntity) (*domain.ProjectEntity, error) {
+	entry := record.ProjectWithoutAutofieldEntry{
+		Name:        project.Name().Value(),
+		Description: project.Description().Value(),
+		UserId:      userId.Value(),
+	}
+
+	key, entryWithTimestamp, err := s.repository.InsertProject(entry)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert project: %w", err)
+	}
+
+	id, err := domain.NewProjectIdObject(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity (id): %w", err)
+	}
+	name, err := domain.NewProjectNameObject(entryWithTimestamp.Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity (name): %w", err)
+	}
+	description, err := domain.NewProjectDescriptionObject(entryWithTimestamp.Description)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity (description): %w", err)
+	}
+	createdAt, err := domain.NewCreatedAtObject(entryWithTimestamp.CreatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity (createdAt): %w", err)
+	}
+	updatedAt, err := domain.NewUpdatedAtObject(entryWithTimestamp.UpdatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity (updatedAt): %w", err)
+	}
+	projectEntity, err := domain.NewProjectEntity(*id, *name, *description, *createdAt, *updatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert entry to entity: %w", err)
+	}
+
+	return projectEntity, nil
 }
--- internal/service/project_test.go
+++ internal/service/project_test.go
@@ -29,21 +29,21 @@ func TestListProjectsValidEntry(t *testing.T) {
 				Name:        maxLengthProjectName,
 				Description: maxLengthProjectDescription,
 				UserId:      testutil.UserId(),
-				CreatedAt:   time.Date(2024, 1, 1, 3, 0, 0, 0, time.UTC),
-				UpdatedAt:   time.Date(2024, 1, 1, 3, 0, 0, 0, time.UTC),
+				CreatedAt:   testutil.Date().Add(3 * time.Hour),
+				UpdatedAt:   testutil.Date().Add(3 * time.Hour),
 			},
 			"0000000000000002": {
 				Name:      "Second Project",
 				UserId:    testutil.UserId(),
-				CreatedAt: time.Date(2024, 1, 1, 2, 0, 0, 0, time.UTC),
-				UpdatedAt: time.Date(2024, 1, 1, 2, 0, 0, 0, time.UTC),
+				CreatedAt: testutil.Date().Add(2 * time.Hour),
+				UpdatedAt: testutil.Date().Add(2 * time.Hour),
 			},
 			"0000000000000001": {
 				Name:        "First Project",
 				Description: "This is my first project",
 				UserId:      testutil.UserId(),
-				CreatedAt:   time.Date(2024, 1, 1, 1, 0, 0, 0, time.UTC),
-				UpdatedAt:   time.Date(2024, 1, 1, 1, 0, 0, 0, time.UTC),
+				CreatedAt:   testutil.Date().Add(1 * time.Hour),
+				UpdatedAt:   testutil.Date().Add(1 * time.Hour),
 			},
 		}, nil)
 
@@ -61,17 +61,22 @@ func TestListProjectsValidEntry(t *testing.T) {
 	assert.Equal(t, "0000000000000001", project.Id().Value())
 	assert.Equal(t, "First Project", project.Name().Value())
 	assert.Equal(t, "This is my first project", project.Description().Value())
+	assert.Equal(t, testutil.Date().Add(1*time.Hour), project.CreatedAt().Value())
+	assert.Equal(t, testutil.Date().Add(1*time.Hour), project.UpdatedAt().Value())
 
 	project = projects[1]
 	assert.Equal(t, "0000000000000002", project.Id().Value())
 	assert.Equal(t, "Second Project", project.Name().Value())
 	assert.Equal(t, "", project.Description().Value())
+	assert.Equal(t, testutil.Date().Add(2*time.Hour), project.CreatedAt().Value())
+	assert.Equal(t, testutil.Date().Add(2*time.Hour), project.UpdatedAt().Value())
 
 	project = projects[2]
 	assert.Equal(t, "0000000000000003", project.Id().Value())
 	assert.Equal(t, maxLengthProjectName, project.Name().Value())
 	assert.Equal(t, maxLengthProjectDescription, project.Description().Value())
-
+	assert.Equal(t, testutil.Date().Add(3*time.Hour), project.CreatedAt().Value())
+	assert.Equal(t, testutil.Date().Add(3*time.Hour), project.UpdatedAt().Value())
 }
 
 func TestListProjectsNoEntry(t *testing.T) {
@@ -108,8 +113,10 @@ func TestListProjectsInvalidEntry(t *testing.T) {
 			name:      "should return error when project id is empty",
 			projectId: "",
 			project: record.ProjectEntry{
-				Name:   "Project",
-				UserId: testutil.UserId(),
+				Name:      "Project",
+				UserId:    testutil.UserId(),
+				CreatedAt: testutil.Date(),
+				UpdatedAt: testutil.Date(),
 			},
 			expectedError: "failed to convert entry to entity (id): project id is required, but got ''",
 		},
@@ -117,8 +124,10 @@ func TestListProjectsInvalidEntry(t *testing.T) {
 			name:      "should return error when project name is empty",
 			projectId: "0000000000000001",
 			project: record.ProjectEntry{
-				Name:   "",
-				UserId: testutil.UserId(),
+				Name:      "",
+				UserId:    testutil.UserId(),
+				CreatedAt: testutil.Date(),
+				UpdatedAt: testutil.Date(),
 			},
 			expectedError: "failed to convert entry to entity (name): project name is required, but got ''",
 		},
@@ -126,8 +135,10 @@ func TestListProjectsInvalidEntry(t *testing.T) {
 			name:      "should return error when project name is too long",
 			projectId: "0000000000000001",
 			project: record.ProjectEntry{
-				Name:   tooLongProjectName,
-				UserId: testutil.UserId(),
+				Name:      tooLongProjectName,
+				UserId:    testutil.UserId(),
+				CreatedAt: testutil.Date(),
+				UpdatedAt: testutil.Date(),
 			},
 			expectedError: "failed to convert entry to entity (name): " +
 				fmt.Sprintf(
@@ -142,6 +153,8 @@ func TestListProjectsInvalidEntry(t *testing.T) {
 				Name:        "Project",
 				Description: tooLongProjectDescription,
 				UserId:      testutil.UserId(),
+				CreatedAt:   testutil.Date(),
+				UpdatedAt:   testutil.Date(),
 			},
 			expectedError: "failed to convert entry to entity (description): " +
 				fmt.Sprintf(
@@ -192,4 +205,186 @@ func TestListProjectsRepositoryError(t *testing.T) {
 	projects, err := s.ListProjects(*userId)
 	assert.ErrorContains(t, err, "failed to fetch user projects: repository error")
 	assert.Nil(t, projects)
+}
+
+func TestCreateProjectValidEntry(t *testing.T) {
+	maxLengthProjectName := testutil.RandomString(100)
+	maxLengthProjectDescription := testutil.RandomString(400)
+
+	tt := []struct {
+		name      string
+		projectId string
+		project   record.ProjectWithoutAutofieldEntry
+	}{
+		{
+			name:      "should return project with valid entry",
+			projectId: "0000000000000001",
+			project: record.ProjectWithoutAutofieldEntry{
+				Name:        "New Project",
+				Description: "This is new project",
+				UserId:      testutil.UserId(),
+			},
+		},
+		{
+			name:      "should return project with max-length valid entry",
+			projectId: "0000000000000002",
+			project: record.ProjectWithoutAutofieldEntry{
+				Name:        maxLengthProjectName,
+				Description: maxLengthProjectDescription,
+				UserId:      testutil.UserId(),
+			},
+		},
+	}
+
+	for _, tc := range tt {
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		r := repository.NewMockProjectRepository(ctrl)
+		r.EXPECT().
+			InsertProject(tc.project).
+			Return(tc.projectId, &record.ProjectEntry{
+				Name:        tc.project.Name,
+				Description: tc.project.Description,
+				UserId:      tc.project.UserId,
+				CreatedAt:   testutil.Date(),
+				UpdatedAt:   testutil.Date(),
+			}, nil)
+
+		s := service.NewProjectService(r)
+
+		userId, err := domain.NewUserIdObject(testutil.UserId())
+		assert.NoError(t, err)
+
+		name, err := domain.NewProjectNameObject(tc.project.Name)
+		assert.NoError(t, err)
+		description, err := domain.NewProjectDescriptionObject(tc.project.Description)
+		assert.NoError(t, err)
+
+		project, err := domain.NewProjectWithoutAutofieldEntity(*name, *description)
+		assert.NoError(t, err)
+
+		createdProject, err := s.CreateProject(*userId, *project)
+		assert.NoError(t, err)
+
+		assert.Equal(t, tc.projectId, createdProject.Id().Value())
+		assert.Equal(t, tc.project.Name, createdProject.Name().Value())
+		assert.Equal(t, tc.project.Description, createdProject.Description().Value())
+		assert.Equal(t, testutil.Date(), createdProject.CreatedAt().Value())
+		assert.Equal(t, testutil.Date(), createdProject.UpdatedAt().Value())
+	}
+}
+
+func TestCreateProjectInvalidCreatedEntry(t *testing.T) {
+	tooLongProjectName := testutil.RandomString(101)
+	tooLongProjectDescription := testutil.RandomString(401)
+
+	tt := []struct {
+		name           string
+		createdProject record.ProjectEntry
+		expectedError  string
+	}{
+		{
+			name: "should return error when project name is empty",
+			createdProject: record.ProjectEntry{
+				Name:        "",
+				Description: "This is new project",
+				UserId:      testutil.UserId(),
+				CreatedAt:   testutil.Date(),
+				UpdatedAt:   testutil.Date(),
+			},
+			expectedError: "failed to convert entry to entity (name): project name is required, but got ''",
+		},
+		{
+			name: "should return error when project name is too long",
+			createdProject: record.ProjectEntry{
+				Name:        tooLongProjectName,
+				Description: "This is new project",
+				UserId:      testutil.UserId(),
+				CreatedAt:   testutil.Date(),
+				UpdatedAt:   testutil.Date(),
+			},
+			expectedError: "failed to convert entry to entity (name): " +
+				fmt.Sprintf(
+					"project name cannot be longer than 100 characters, but got '%s'",
+					tooLongProjectName,
+				),
+		},
+		{
+			name: "should return error when project description is too long",
+			createdProject: record.ProjectEntry{
+				Name:        "New Project",
+				Description: tooLongProjectDescription,
+				UserId:      testutil.UserId(),
+				CreatedAt:   testutil.Date(),
+				UpdatedAt:   testutil.Date(),
+			},
+			expectedError: "failed to convert entry to entity (description): " +
+				fmt.Sprintf(
+					"project description cannot be longer than 400 characters, but got '%s'",
+					tooLongProjectDescription,
+				),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			r := repository.NewMockProjectRepository(ctrl)
+			r.EXPECT().
+				InsertProject(record.ProjectWithoutAutofieldEntry{
+					Name:        "New Project",
+					Description: "This is new project",
+					UserId:      testutil.UserId(),
+				}).
+				Return("0000000000000001", &tc.createdProject, nil)
+
+			s := service.NewProjectService(r)
+
+			userId, err := domain.NewUserIdObject(testutil.UserId())
+			assert.NoError(t, err)
+
+			name, err := domain.NewProjectNameObject("New Project")
+			assert.NoError(t, err)
+			description, err := domain.NewProjectDescriptionObject("This is new project")
+			assert.NoError(t, err)
+
+			project, err := domain.NewProjectWithoutAutofieldEntity(*name, *description)
+			assert.NoError(t, err)
+
+			createdProject, err := s.CreateProject(*userId, *project)
+			assert.ErrorContains(t, err, tc.expectedError)
+			assert.Nil(t, createdProject)
+		})
+	}
+}
+
+func TestCreateProjectRepositoryError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := repository.NewMockProjectRepository(ctrl)
+	r.EXPECT().
+		InsertProject(gomock.Any()).
+		Return("", nil, fmt.Errorf("repository error"))
+
+	s := service.NewProjectService(r)
+
+	userId, err := domain.NewUserIdObject(testutil.UserId())
+	assert.NoError(t, err)
+
+	name, err := domain.NewProjectNameObject("New Project")
+	assert.NoError(t, err)
+	description, err := domain.NewProjectDescriptionObject("This is new project")
+	assert.NoError(t, err)
+
+	project, err := domain.NewProjectWithoutAutofieldEntity(*name, *description)
+	assert.NoError(t, err)
+
+	createdProject, err := s.CreateProject(*userId, *project)
+	assert.ErrorContains(t, err, "failed to insert project: repository error")
+	assert.Nil(t, createdProject)
 }
