c490e23d288a4ab05a5131b35d608e9c370a6013
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-03-03 17:19:10 +0900

    implement api

--- internal/api/project.go
+++ internal/api/project.go
@@ -10,6 +10,7 @@ import (
 
 type ProjectApi interface {
 	HandleList(c *gin.Context)
+	HandleFind(c *gin.Context)
 	HandleCreate(c *gin.Context)
 }
 
@@ -37,6 +38,44 @@ func (api projectApi) HandleList(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, model.ProjectListErrorResponse{
 			Message: UseCaseErrorToMessage(ucErr),
 			User:    resErr.User,
+		})
+		return
+	}
+
+	if ucErr != nil {
+		c.JSON(http.StatusInternalServerError, model.ApplicationErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func (api projectApi) HandleFind(c *gin.Context) {
+	var request model.ProjectFindRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, model.ProjectFindErrorResponse{
+			Message: JsonBindErrorToMessage(err),
+		})
+		return
+	}
+
+	res, ucErr := api.usecase.FindProject(request)
+
+	if ucErr != nil && ucErr.Code() == usecase.InvalidArgumentError {
+		resErr := UseCaseErrorToResponse(ucErr)
+		c.JSON(http.StatusBadRequest, model.ProjectFindErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
+			User:    resErr.User,
+			Project: resErr.Project,
+		})
+		return
+	}
+
+	if ucErr != nil && ucErr.Code() == usecase.NotFoundError {
+		c.JSON(http.StatusNotFound, model.ProjectFindErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
 		})
 		return
 	}
