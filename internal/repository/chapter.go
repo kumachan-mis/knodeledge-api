6d2b678839b301d1e048ddc7ef19d02bdf006794
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-03-31 13:10:45 +0900

    implement repository

---
+++ internal/repository/chapter.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"cloud.google.com/go/firestore"
+	"github.com/kumachan-mis/knodeledge-api/internal/db"
+	"github.com/kumachan-mis/knodeledge-api/internal/record"
+)
+
+const ChapterCollection = "chapters"
+
+type ChapterRepository interface {
+	FetchProjectChapters(projectId string) (map[string]record.ChapterEntry, *Error)
+}
+
+type chapterRepository struct {
+	client firestore.Client
+}
+
+func NewChapterRepository(client firestore.Client) ChapterRepository {
+	return chapterRepository{client: client}
+}
+
+func (r chapterRepository) FetchProjectChapters(projectId string) (map[string]record.ChapterEntry, *Error) {
+	docref := r.client.Collection(ProjectCollection).
+		Doc(projectId)
+
+	if _, err := docref.Get(db.FirestoreContext()); err != nil {
+		return nil, Errorf(NotFoundError, "parent collection not found")
+	}
+
+	iter := docref.
+		Collection(ChapterCollection).
+		Documents(db.FirestoreContext())
+
+	entries := make(map[string]record.ChapterEntry)
+
+	for {
+		snapshot, err := iter.Next()
+		if err != nil {
+			break
+		}
+
+		var entry record.ChapterEntry
+		err = snapshot.DataTo(&entry)
+		if err != nil {
+			return nil, Errorf(ReadFailurePanic, "failed to convert snapshot to entry: %w", err)
+		}
+
+		entries[snapshot.Ref.ID] = entry
+	}
+
+	return entries, nil
+}
