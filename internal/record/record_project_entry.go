d66b4a2ca3a364bce9bd0f39e77d2cfb2ca3ecf9
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-04-06 18:06:42 +0900

    refactor: create document package

--- internal/record/record_project_entry.go
+++ internal/record/record_project_entry.go
@@ -3,9 +3,9 @@ package record
 import "time"
 
 type ProjectEntry struct {
-	Name        string    `firestore:"name"`
-	Description string    `firestore:"description,omitempty"`
-	UserId      string    `firestore:"userId"`
-	CreatedAt   time.Time `firestore:"createdAt"`
-	UpdatedAt   time.Time `firestore:"updatedAt"`
+	Name        string
+	Description string
+	UserId      string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
