11e9b2b1b944e02dd6f7e70868b506bb39fdb24b
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-09-08 00:01:55 +0900

    imp api

---
+++ internal/api/graph.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kumachan-mis/knodeledge-api/internal/model"
+	"github.com/kumachan-mis/knodeledge-api/internal/usecase"
+)
+
+type GraphApi interface {
+	HandleSectionalize(c *gin.Context)
+}
+
+type graphApi struct {
+	usecase usecase.GraphUseCase
+}
+
+func NewGraphApi(usecase usecase.GraphUseCase) GraphApi {
+	return graphApi{usecase: usecase}
+}
+
+func (api graphApi) HandleSectionalize(c *gin.Context) {
+	var request model.GraphSectionalizeRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, model.GraphSectionalizeErrorResponse{
+			Message: JsonBindErrorToMessage(err),
+		})
+		return
+	}
+
+	res, ucErr := api.usecase.SectionalizeGraph(request)
+
+	if ucErr != nil && ucErr.Code() == usecase.DomainValidationError {
+		resErr := UseCaseErrorToResponse(ucErr)
+		c.JSON(http.StatusBadRequest, model.GraphSectionalizeErrorResponse{
+			Message:  UseCaseErrorToMessage(ucErr),
+			User:     resErr.User,
+			Project:  resErr.Project,
+			Chapter:  resErr.Chapter,
+			Sections: resErr.Sections,
+		})
+		return
+	}
+
+	if ucErr != nil && ucErr.Code() == usecase.InvalidArgumentError {
+		c.JSON(http.StatusBadRequest, model.GraphSectionalizeErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
+		})
+		return
+	}
+
+	if ucErr != nil && ucErr.Code() == usecase.NotFoundError {
+		c.JSON(http.StatusNotFound, model.GraphSectionalizeErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
+		})
+		return
+	}
+
+	if ucErr != nil {
+		c.JSON(http.StatusInternalServerError, model.ApplicationErrorResponse{
+			Message: UseCaseErrorToMessage(ucErr),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, res)
+}
